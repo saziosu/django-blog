a363a3547b74e0f2af9fcac969b34fb371f1f676
saziosu <>
2023-10-10 19:58:08 +0000

    Add models and admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    # allow slug to be pre-populated from the post title
+    prepopulated_fields = {'slug': ('title',)}
+    # add filter option to the admin panel
+    list_filter = ('status', 'created_on')
+    # display these items on the posts in admin panel
+    list_display = ('title', 'slug', 'status', 'created_on')
+    # allow these terms to be searched in admin panel
+    search_fields = ['title', 'content']
+    # add summernote editor to the post content field
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    # allow comments to be approved from admin panel view
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.updated(approved=True)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+# class based views making them re-usable, so try to use
+# classes over functions
+class PostList(generic.ListView):
+    model = Post
+    # filter posts, display only published posts to users
+    # display in descending order - oldest first
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+    
